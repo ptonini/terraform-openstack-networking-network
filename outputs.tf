da0b147ee891066d3eb7dd43d0d3a6e99ed86b6e
ptonini <pedro.tonini@hotmail.com>
2023-11-06 16:43:40 -0300

    feat: added router to output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "this" {
   value = openstack_networking_network_v2.this
 }
 
+output "router" {
+  value = openstack_networking_router_v2.this
+}
+
+
 output "subnets" {
   value = openstack_networking_subnet_v2.this
 }
